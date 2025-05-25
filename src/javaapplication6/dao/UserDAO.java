1741d3c15f4ed5286bda9684595c608c051c1430
imnb57 <imnitesh57@gmail.com>
2025-05-25 12:01:10 +0545

    Dashboard ui commited

--- src/javaapplication6/dao/UserDAO.java
+++ src/javaapplication6/dao/UserDAO.java
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package javaapplication6.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import javaapplication6.database.DBConn;
 import javaapplication6.model.RegisterModel;
-
-import java.sql.*;
 import javaapplication6.HashUtil.HashUtil;
-import javax.swing.JOptionPane;
 
-/**
- *
- * @author suhritsatyal
- */
 
 public class UserDAO {
     private final DBConn dbConn;
@@ -44,50 +34,4 @@ public class UserDAO {
 
         return result;
     }
-//    public boolean loginUser(LoginModel loginmodel)  //backend developer le controller ra model banayechi yo error jancha, for the time being there is error.
-=======
-/////*
-//// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-//// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-//// */
-//package javaapplication6.dao;
-//
-//import javaapplication6.database.DBConn;
-//import javaapplication6.model.RegisterModel;
-//
-////
-/////**
-//// *
-//// * @author suhritsatyal
-//// */
-//public class UserDAO {
-//    
-//    
-//    public boolean registerUser(RegisterModel registermodel)
->>>>>>> manoj
-//    {
-//        DBConn conn=new DBConn();
-//        String sql_query="SELECT * FROM users_table where email=? and password=?";
-//        try {
-//            PreparedStatement pstmt=(PreparedStatement) conn.prepareStatement(sql_query);
-//            pstmt.setString(1, loginmodel.getEmail());
-//            pstmt.setString(2, loginmodel.getPassword());
-//            if(pstmt.executeQuery().next())
-//            {
-//                return true;
-//            }
-//            else
-//            {
-//                return false;
-//            }
-//        } catch (Exception e) {
-//            return false;
-//        }
-//    }
-<<<<<<< HEAD
 }
-=======
-//    
-//    
-//}
->>>>>>> manoj
