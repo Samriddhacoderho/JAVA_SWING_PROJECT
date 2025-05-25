5f39f989f2285d7b93ff2d51e3a892cee2f5bb80
imnb57 <imnitesh56@gmail.com>
2025-05-25 12:01:10 +0545

    Dashboard ui commited

--- src/javaapplication6/view/HomeView.java
+++ src/javaapplication6/view/HomeView.java
@@ -98,6 +98,7 @@ public class HomeView extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
