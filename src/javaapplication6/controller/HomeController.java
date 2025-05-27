c2e602d77c5efadba99327611243673887f89bfe
Samriddha Raj Satyal <130677288+Samriddhacoderho@users.noreply.github.com>
2025-05-27 10:17:09 +0545

    Update HomeController.java

--- src/javaapplication6/controller/HomeController.java
+++ src/javaapplication6/controller/HomeController.java
@@ -39,6 +39,7 @@ public class HomeController {
             RegistrationController registrationController=new RegistrationController(registrationView);
             registrationController.open();
             close();
+            //ok
             
         }
 
