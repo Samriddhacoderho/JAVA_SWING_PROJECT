c8e8a2fd6c04d06b186e574528000e7b3bd4238a
Yuri9828835868 <135593196+Yuri9828835868@users.noreply.github.com>
2025-05-27 11:14:29 +0545

    Login Controller

---
+++ src/javaapplication6/model/LoginModel.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication6.model;
+
+/**
+ *
+ * @author Yuri Maharjan
+ */
+public class LoginModel {
+    private String email;
+    private String password;
+    
+    public LoginModel(String email,String password)
+    {
+        this.email=email;
+        this.password=password;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
