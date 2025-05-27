01eed9172e2e241577eb4999b465e8dcdc3477e8
Yuri9828835868 <135593196+Yuri9828835868@users.noreply.github.com>
2025-05-27 11:14:29 +0545

    Login Controller

--- src/javaapplication6/dao/UserDAO.java
+++ src/javaapplication6/dao/UserDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import javaapplication6.database.DBConn;
 import javaapplication6.model.RegisterModel;
 import javaapplication6.HashUtil.HashUtil;
+import javaapplication6.model.LoginModel;
 
 
 public class UserDAO {
@@ -35,24 +36,24 @@ public class UserDAO {
         return result;
     }
     
-//    public boolean loginUser(LoginModel loginmodel)  //backend developer le controller ra model banayechi yo error jancha, for the time being there is error.
-//    {
-//        DBConn conn=new DBConn();
-//        String sql_query="SELECT * FROM users_table where email=? and password=?";
-//        try {
-//            PreparedStatement pstmt=conn.prepareStatement(sql_query);
-//            pstmt.setString(1, loginmodel.getEmail());
-//            pstmt.setString(2, loginmodel.getPassword());
-//            if(pstmt.executeQuery().next())
-//            {
-//                return true;
-//            }
-//            else
-//            {
-//                return false;
-//            }
-//        } catch (Exception e) {
-//            return false;
-//        }
-//    }
+    public boolean loginUser(LoginModel loginmodel)  //backend developer le controller ra model banayechi yo error jancha, for the time being there is error.
+    {
+        DBConn conn=new DBConn();
+        String sql_query="SELECT * FROM users_table where email=? and password=?";
+        try {
+            PreparedStatement pstmt=conn.prepareStatement(sql_query);
+            pstmt.setString(1, loginmodel.getEmail());
+            pstmt.setString(2, loginmodel.getPassword());
+            if(pstmt.executeQuery().next())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
