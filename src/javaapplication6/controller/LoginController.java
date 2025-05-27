a404ceaf76c2036a2ad964ea107caf0f840ce510
Yuri9828835868 <135593196+Yuri9828835868@users.noreply.github.com>
2025-05-27 11:14:29 +0545

    Login Controller

--- src/javaapplication6/controller/LoginController.java
+++ src/javaapplication6/controller/LoginController.java
@@ -1,43 +1,115 @@
-//
-/////*
-//// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-//// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-//// */
-//package javaapplication6.controller;
-//
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
-//import javax.swing.JOptionPane;
-//
-/////**
-//// *
-//// * @author Dell
-//// */
-//public class LoginController {
-//    private LoginView loginview = new LoginView();
-////    code here for controlling opening and closing of view
-//
-//    
-//    
-////    code for validation of user during login
-//   
-//    class userLogin implements ActionListener{
-//        @Override
-//        public void actionPerformed(ActionEvent e){
-//            String email = loginView.getEmailTextField.getText();
-//            String password = String.valueOf(loginView.getPasswordField()).getPassword();
-//            if(email.isEmpty()||password.isEmpty()){
-//                JOptionPane.showMessageDialog(loginView,"Fields cannot be empty!");
-//            }
-//            else{
-//                JOptionPane.showMessageDialog(loginView, "Details are validated");
-//            }
-//            
-//        }
-//        
-//    }
-//    
-//    
-//}
-//
-//
+package javaapplication6.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javaapplication6.dao.UserDAO;
+import javax.swing.JOptionPane;
+import javaapplication6.model.LoginModel;
+import javaapplication6.view.HomeView;
+import javaapplication6.view.LoginView;
+import javaapplication6.view.RegistrationView;
+// import javaapplication6.dao.UserDAO; // optional for DB
+public class LoginController {
+
+    private final LoginView loginView;
+    private final UserDAO userDAO=new UserDAO();
+    private Object view;
+
+    public LoginController(LoginView loginView) {
+        this.loginView = loginView;
+        loginView.LoginActionListener(new LoginUser());
+        loginView.ShowActionListener(new ShowPassword());
+    }
+
+    public void open() {
+        loginView.setVisible(true);
+    }
+
+    public void close() {
+        loginView.dispose();
+    }
+
+    private void addMouseListener(MouseListener listener) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    class LoginUser implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String email = loginView.getEmailTxt().getText();
+            String password = new String(loginView.getPassPsf().getText());
+            
+            //validation part
+            String validation = validate(email, password);
+            
+            
+            if (!validation.equals("Logging in...")) {
+                JOptionPane.showMessageDialog(loginView, validation);
+            } else {
+                try {
+                    LoginModel loginModel=new LoginModel(email,password);
+                    boolean result=userDAO.loginUser(loginModel);
+
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(loginView, "Error: " + ex.getMessage());
+                }
+            }
+        }  
+        
+         public String validate(String email, String password) {
+            if (email.isEmpty() || password.isEmpty()) {
+                return "Please enter both email and password";
+            }
+            return "Logging in...";
+        }
+        }
+    
+    class ShowPassword implements ActionListener
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(loginView.getShowBtn().getText()=="Show")
+                {
+                    loginView.getShowBtn().setText("Hide");
+                    loginView.getPassPsf().setEchoChar((char) 0);
+                }
+                else
+                {
+                    loginView.getShowBtn().setText("Show");
+                    loginView.getPassPsf().setEchoChar('*' );
+                }
+            }   
+    }
+    
+    class SignupListener implements MouseListener
+    {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+             RegistrationView registrationView=new RegistrationView();
+            RegistrationController registrationController=new RegistrationController(registrationView);
+            registrationController.open();
+            close();
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+        }
+        
+    }
+    }
