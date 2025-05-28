6bd816c7ac1b5a1de24bf0d9cf749414b5f0eeff
imnb57 <imnitesh57@gmail.com>
2025-05-28 19:24:00 +0545

    Change Password Controller and Dashboard Controller implemented

---
+++ src/javaapplication6/controller/ChangePasswordController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication6.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javaapplication6.view.ChangePassView;
+
+
+/**
+ *
+ * @author Dell
+ */
+public class ChangePasswordController {
+    private final ChangePassView changePass;
+    
+    public ChangePasswordController(ChangePassView changePass){
+        this.changePass = changePass;
+        changePass.PasswordChangeConfirm(new ChangePassListener());
+        
+    }
+    public void open(){
+        this.changePass.setVisible(true);
+    }
+    public void close(){
+        this.changePass.dispose();
+    }
+    
+    class ChangePassListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+            
+            
+            
+        }
+    }
+}
