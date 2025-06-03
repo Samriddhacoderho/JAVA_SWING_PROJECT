359317bcc7760d825f6736c13c91d5620cd44a4c
Samriddha R. Satyal <satyalsamriddha@gmail.com>
2025-06-03 20:02:37 +0545

    Test

--- src/javaapplication6/controller/EnterEmailController.java
+++ src/javaapplication6/controller/EnterEmailController.java
@@ -73,10 +73,12 @@ public class EnterEmailController {
         public void actionPerformed(ActionEvent e) {
             String email=enterEmailView.getEmailTxt().getText();
             EnterEmailModel enterEmailModel=new EnterEmailModel(email);
+            //dao lai call garna baki
             EnterOTPView enterOTPView=new EnterOTPView();
             EnterOTPController enterOTPController=new EnterOTPController(enterOTPView);
             enterOTPController.open();
             close();
+            
         }
         
     }
