d384b2b3a8669bf2f5c1a2ba2146264670c667b2
Samriddha R. Satyal <satyalsamriddha@gmail.com>
2025-06-03 20:25:04 +0545

    OTP Controller Done, and mailing also done

--- src/javaapplication6/controller/EnterOTPController.java
+++ src/javaapplication6/controller/EnterOTPController.java
@@ -4,7 +4,12 @@
  */
 package javaapplication6.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javaapplication6.controller.Mail.SMTPSMailSender;
+import javaapplication6.model.ResetModel;
 import javaapplication6.view.EnterOTPView;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -13,9 +18,13 @@ import javaapplication6.view.EnterOTPView;
 
 public class EnterOTPController {
     private final EnterOTPView enterOTPView;
-    public EnterOTPController(EnterOTPView enterOTPView)
+    private String email;
+    private SMTPSMailSender smtpsMailSender;
+    public EnterOTPController(EnterOTPView enterOTPView,String email)
     {
         this.enterOTPView=enterOTPView;
+        this.email=email;
+        this.enterOTPView.VerifyActionListener(new VerifyListener());
     }
     
     public void open()
@@ -27,4 +36,43 @@ public class EnterOTPController {
     {
         this.enterOTPView.dispose();
     }
+    
+    class VerifyListener implements ActionListener
+    {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int random_otp=(int) (Math.random()*(9999-1000))+1000;
+            String body="The otp to reset your password is:"+random_otp;
+            boolean mailSent=smtpsMailSender.sendMail(email, "Reset Password Verification", body);
+            if(!mailSent)
+            {
+                       JOptionPane.showMessageDialog(enterOTPView, "Faialed to send OTP. Please try again later!");
+            }
+            else
+            {
+                String user_otp=enterOTPView.getOtpTxt().getText();
+                if(!user_otp.equals(String.valueOf(random_otp)))
+                {
+                    JOptionPane.showMessageDialog(enterOTPView, "Incorrect OTP Code, please try again");
+                }
+                else
+                {
+                    String password=JOptionPane.showInputDialog(email,"Enter your new password:");
+                           if(password.trim().isEmpty())
+                           {
+                               JOptionPane.showMessageDialog(enterOTPView, "Password cannot be empty!");
+                           }
+                           else
+                           {
+                               ResetModel resetModel=new ResetModel(email, password);
+                               //dao wala le banayechi ill call
+                           }
+                }
+            }
+        }
+        
+    }
+    
+    
 }
