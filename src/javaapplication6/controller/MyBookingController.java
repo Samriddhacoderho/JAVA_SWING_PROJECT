d9f2eb33a20192c56390235bd45e84d6ae164d8e
imnb57 <=imnitesh56@gmail.com>
2025-06-10 11:14:28 +0545

    view booking controller

--- src/javaapplication6/controller/MyBookingController.java
+++ src/javaapplication6/controller/MyBookingController.java
@@ -5,6 +5,10 @@
 package javaapplication6.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javaapplication6.model.LoginModel;
+import javaapplication6.view.DashboardView;
 import javaapplication6.view.MyBookings;
 
 /**
@@ -13,9 +17,14 @@ import javaapplication6.view.MyBookings;
  */
 public class MyBookingController {
    private final MyBookings bookingView;
+   private final LoginModel loginModel;
    
-   public MyBookingController(MyBookings bookingView){
+   
+   public MyBookingController(MyBookings bookingView,LoginModel loginModel){
        this.bookingView = bookingView;
+       this.loginModel=loginModel;
+      bookingView.ProfileListener(new BacktoProfile());
+     bookingView.ViewBookingListener(new CancelBooking());
        
    }
     
@@ -30,6 +39,35 @@ public class MyBookingController {
         @Override
         public void actionPerformed(ActionEvent e) {
             
+        }
+        
+    }
+    class BacktoProfile implements MouseListener{
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            DashboardView dash = new DashboardView();
+            DashboardController dashController = new DashboardController(dash,loginModel);
+            dashController.open();
+            close();
+            
+            
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
         }
         
     }
