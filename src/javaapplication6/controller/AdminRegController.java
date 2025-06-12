558754ba5d47cb8e2140b051ece57675139a5cc4
Samriddha R. Satyal <satyalsamriddha@gmail.com>
2025-06-12 20:29:34 +0545

    Some Logic in Register Admin and nav logic

--- src/javaapplication6/controller/AdminRegController.java
+++ src/javaapplication6/controller/AdminRegController.java
@@ -11,47 +11,70 @@ import java.awt.event.MouseListener;
 import javaapplication6.dao.UserDAO;
 import javaapplication6.dao.VenueManagerDAO;
 import javaapplication6.model.RegisterModel;
+import javaapplication6.view.AdminRegisterView;
 import javaapplication6.view.LoginView;
-import javaapplication6.view.RegistrationView;
+import javaapplication6.view.UserRegisterView;
 import javax.swing.JOptionPane;
 
 
 public class AdminRegController {
     
-//    private final RegistrationView registrationView;
+    private final AdminRegisterView view;
     private final VenueManagerDAO dao=new VenueManagerDAO();
-    public AdminRegController(){
-        
+    public AdminRegController(AdminRegisterView view) {
+        this.view=view;
+        view.RegisterUserListener(new RegisterUser());
+        view.showPasswordListener(new showPassword());
+        view.showConfirmPasswordListener(new showConfirmPassword());
+        view.LoginNavigate(new LoginNavigationListener());
     }
     
     
     public void open()
     {
+        this.view.setVisible(true);
     }
     
     public void close()
     {
+        this.view.dispose();
     }
     
      class showPassword implements ActionListener
         {
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        }
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(view.getPassShowbtn().getText()=="Show")
+                {
+                    view.getPassShowbtn().setText("Hide");
+                    view.getPassPsf().setEchoChar((char) 0);
+                }
+                else
+                {
+                    view.getPassShowbtn().setText("Show");
+                    view.getPassPsf().setEchoChar('*');
+                }
+            }
             
         }
      
      class showConfirmPassword implements ActionListener
         {
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        }
-
-           
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(view.getConfirmPassshowbtn().getText()=="Show")
+                {
+                    view.getConfirmPassshowbtn().setText("Hide");
+                    view.getConfirmPsf().setEchoChar((char) 0);
+                }
+                else
+                {
+                    view.getConfirmPassshowbtn().setText("Show");
+                    view.getConfirmPsf().setEchoChar('*');
+                }
+            }
             
         }
      class RegisterUser implements ActionListener
@@ -59,10 +82,63 @@ public class AdminRegController {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            String name=view.getNameTxt().getText();
+            String email=view.getEmailTxt().getText();
+            String password=view.getPassPsf().getText();
+            String confirmPassword=view.getConfirmPsf().getText();
+            
+            //validation part:
+            String validate_result=validate(name, email, password, confirmPassword);
+            if(!validate_result.equals("Registering Venue Manager..."))
+            {
+                JOptionPane.showMessageDialog(view, validate_result);
+            }
+            else
+            {
+                if(JOptionPane.showConfirmDialog(view, "Are you sure you want to register?")==0)
+                {
+                try {
+                    RegisterModel registerModel=new RegisterModel(name, email, password, confirmPassword);
+                    boolean result=dao.registerVM(registerModel);
+                    if(result)
+                    {
+                        JOptionPane.showMessageDialog(view, "Registration Successful");
+                        LoginView loginView=new LoginView();
+                        LoginController loginController=new LoginController(loginView);
+                        loginController.open();
+                        close();
+                    }
+                    else
+                    {
+                        JOptionPane.showMessageDialog(view, "Registration Failed");
+                    }
+                } catch (Exception error) {
+                    System.out.println(error.getMessage());
+                }
+                }
+            }
+            
         }
-
-      
+        
+        public static String validate(String name,String email,String password,String confirmPassword)
+        {
+            if(name.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty())
+            {
+                return "You are required to fill all the fields";
+            }
+            else if(!password.equals(confirmPassword))
+            {
+                return "Passwords do not match each other";
+            }
+            else if(password.length()<=8 || password.length()>=16)
+            {
+                return "Password is either too short or too long";
+            }
+            else
+            {
+                return "Registering User...";
+            }
+        }
         
         
     }
@@ -72,29 +148,27 @@ public class AdminRegController {
 
         @Override
         public void mouseClicked(MouseEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            LoginView loginView=new LoginView();
+            LoginController loginController=new LoginController(loginView);
+            loginController.open();
+            close();
         }
 
         @Override
         public void mousePressed(MouseEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
         }
 
         @Override
         public void mouseEntered(MouseEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
         }
 
         @Override
         public void mouseExited(MouseEvent e) {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
         }
-
-       
-}
+         
+     }
 }
--- src/javaapplication6/dao/VenueManagerDAO.java
+++ src/javaapplication6/dao/VenueManagerDAO.java
@@ -4,10 +4,23 @@
  */
 package javaapplication6.dao;
 
+import javaapplication6.database.DBConn;
+import javaapplication6.model.RegisterModel;
+
 /**
  *
  * @author ishan-college
  */
 public class VenueManagerDAO {
+    private final DBConn dbConn;
     
+    public VenueManagerDAO() {
+        dbConn = new DBConn();
+    }
+    
+    public boolean registerVM(RegisterModel model)
+    {
+        return true;
+        //write dao logic here
+    }
 }
