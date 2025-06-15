607f29ab100697b1da66e5f44bdd1906a671d65c
Samriddha R. Satyal <satyalsamriddha@gmail.com>
2025-06-15 19:24:11 +0545

    View bookings from admin side working

--- src/javaapplication6/dao/RegisterVenueDAO.java
+++ src/javaapplication6/dao/RegisterVenueDAO.java
@@ -65,23 +65,25 @@ public class RegisterVenueDAO {
         return false;
     }
 }
-    public boolean getVenue_inVenueManager(String email)
+    
+    public VenueDetailsFetchModel adminVenueView(String email)
     {
-        String sqlQuery="select * from book_details join venue_table on venue_table.id=book_details.venue_id where book_details.email=?";
+        String sqlQuery="select * from book_details join venue_table on venue_table.id=book_details.venue_id where venue_table.email=?";
         try (Connection conn=dbConn.connection_base()){
             PreparedStatement pstmt=conn.prepareStatement(sqlQuery);
             pstmt.setString(1, email);
             var rs=pstmt.executeQuery();
             if(rs.next())
             {
-                VenueDetailsFetchModel result=new VenueDetailsFetchModel(rs.getInt("venue_id"), email, rs.getString("name"), rs.getString("location"), rs.getString("email"), rs.getString("contact_number"), rs.getInt("estimated_guests"), rs.getString("status"), rs.getDouble("price_per_plate"), rs.getLong("total_price"));
-                return true;
+                VenueDetailsFetchModel result=new VenueDetailsFetchModel(rs.getInt("venue_id"), rs.getString("user_email"), rs.getString("name"), rs.getString("location"),email, rs.getString("contact_number"), rs.getInt("estimated_guests"), rs.getString("status"), rs.getDouble("price_per_plate"), rs.getLong("total_price"));
+                return result;
             }
         } catch (Exception e) {
-            return false;  //updation required
+            return null;
         }
-        return false;  //updation required
-    }  
+        return null;
+    }
+    
     public boolean registerVenue(VenueModel model){
         boolean result= false;
         String sql = "insert into venue_table (name, location, email, contact_number,price_per_plate)values(?,?,?,?,?)";
